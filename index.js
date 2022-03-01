229136ec333b857b817f0fe1819b0d662571bc85
Valencia White <valenciawhite@rocketmail.com>
2022-03-01 03:25:22 -0500

    commit tree 762d5d3f16b61c65116d816253fb6a828ae69a26

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
-}
+// Elements needed from the DOM
+// const input = document.getElementById('input').addEventListener('click', e => console.log('hi'));
+
+const input = document.getElementById('input');
+input.addEventListener('click', function(){
+    alert('I was clicked!')
+});
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+
+
+
+
+
+
